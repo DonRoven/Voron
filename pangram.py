fd8180db4ec7af900468b4ec28d1acfb8e7ccf80
Vitalik <si_vitalik@yahoo.com>
2016-08-23 21:30:08 +0300

    added full althabet

--- pangram.py
+++ pangram.py
@@ -3,4 +3,5 @@ from string import ascii_lowercase as az
 def cheak(text):
 	return set(text.lower()).issuperset(set(az))
 
-print (cheak('abc'))
+print (cheak('abc') == False)
+print (cheak('abcdefghijklmnopqrstuvwxyz'))
